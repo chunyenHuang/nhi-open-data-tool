852a27a274643bffbc1b25037a3c7be850a723dd
John Huang <little78926@gmail.com>
2020-08-09 00:11:45 -0700

    Fix missing scripts

--- data/config.js
+++ data/config.js
@@ -1,7 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const sources = require('./sources.json');
 const latestDir = path.join(__dirname, 'latest');
 const backupDir = path.join(__dirname, 'backup');
 
@@ -12,6 +11,5 @@ module.exports = {
   FORCE_CREATE: (process.env.FORCE_CREATE) ? true : false,
   LATEST_DIR: latestDir,
   BACKUP_DIR: backupDir,
-  SOURCES: sources,
   TIME_ZONE: 'Asia/Taipei',
 };
